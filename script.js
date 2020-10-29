08737aeafe28838155828bac92ce0b6b7637b5a0
Narasimha2906 <ynprasadkklreddy2@gmail.com>
2020-10-29 20:34:20 +0530

    g

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let button= document.querySelector("button");
+
+button.addEventListener("click",locstorage);
+
+class gggg{
+    constructor(amount,text){
+        this.amount = amount;
+        //this.id = localStorage.getItem(key)
+        this.text = text;
+
+    }
+  
+}
+
+function locstorage(){
+    var gg = document.getElementById('amount').value;
+    var tt = document.getElementById('text').value;
+    localStorage.setItem(tt,gg)
+    var om = new gggg(gg,tt)
+    console.log(om)
+  
+    
+}
 
